b12d8442921b85b8a5bf95e8ea984fb8b646ace2
Wendy Harris <wendyv510@hotmail.com>
2019-09-22 19:24:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,10 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  {
+    :name => "Grace Hopper"
+    :1 => 2 
+  }
 end
 
 def read_from_hash(hash, key)
